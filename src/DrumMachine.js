e47e58bb3c8eb46006bde3d8271e9fd9aa620119
markoter <markoterdev@outlook.com>
2022-09-14 12:25:07 +0200

    start with adding samples

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Display from "./Display";
+import Pad from "./Pad";
 
 const DrumMachine = () => {
+    const padID = [0,1,2,3,4,5,6,7,8]
+    const samples =  [
+        {
+            name: "Heater 1",
+            src: "./samples/Heater-1.mp3",
+            key: "Q"
+        },
+        
+        ]
+
+
     return (
         <div id="drum-machine">
             and this is drum machine
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
+            <Pad />
             <Display />
         </div>
     )
---
+++ src/Pad.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Pad = () => {
+    return (
+        <div className="drum-pad">
+            drum pad
+        </div>
+    )
+}
+export default Pad
