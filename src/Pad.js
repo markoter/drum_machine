afb6eb6da34dd4ac56d59f1596524f0e3661fc93
markoter <markoterdev@outlook.com>
2022-09-14 20:22:58 +0200

    blinking pad color working

--- src/Pad.js
+++ src/Pad.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Pad = (props) => {
     const { id, sampleSrc, keySelect } = props
+        const activateButton = (button) => {
+            button.classList.toggle("clicked")
+
+        }
     const handleClick = () => {
         const sound = document.querySelector(`#${keySelect}`)
         const button = document.querySelector(`#${id}`)
         sound.play()
-        button.classList.toggle("clicked")
+        activateButton(button)
+        setTimeout(() => activateButton(button), 100)
+
 
     }
     return (
