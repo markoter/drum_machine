5820d5d84c2df6b8a957a58e5edde7c2f32c6c15
markoter <markoterdev@outlook.com>
2022-09-14 19:13:00 +0200

    change pad color when clicked

--- src/Pad.js
+++ src/Pad.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Pad = (props) => {
-    const {id, sampleSrc, keySelect} = props
+    const { id, sampleSrc, keySelect } = props
+    const handleClick = () => {
+        //const sound = document.querySelector(".audio")
+        //sound.play()
+        const button = document.querySelector(`#${id}`)
+        button.classList.toggle("clicked")
+
+    }
     return (
-        <div id={id} className="drum-pad">
-            <p>{keySelect}</p>
+        <div id={id} className="drum-pad" onClick={handleClick}>
+            <p>{keySelect} {id}</p>
+            <audio src={sampleSrc} id={keySelect} className="audio" />
         </div>
     )
 }
