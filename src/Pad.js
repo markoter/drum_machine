7a18babb3955993987e89f564cbfe83459ebfee5
markoter <markoterdev@outlook.com>
2022-09-15 16:29:13 +0200

    keypad play corrct sound but not choose button

--- src/Pad.js
+++ src/Pad.js
@@ -2,19 +2,27 @@ import React from "react";
 
 const Pad = (props) => {
     const { id, sampleSrc, keySelect } = props
-        const activateButton = (button) => {
-            button.classList.toggle("clicked")
+    const activateButton = (button) => {
+        button.classList.toggle("clicked")
 
-        }
+    }
     const handleClick = () => {
         const sound = document.querySelector(`#${keySelect}`)
         const button = document.querySelector(`#${id}`)
         sound.play()
         activateButton(button)
         setTimeout(() => activateButton(button), 100)
-
-
     }
+    const handleKey = (key) => {
+        const keyPressed = '#' + key.toString().toUpperCase()
+        const sound = document.querySelector(keyPressed)
+        const button = document.querySelector(`#${id}`)
+        sound.play()
+        activateButton(button)
+        setTimeout(() => activateButton(button), 100)
+    }
+    window.addEventListener("keyup", (event) => handleKey(event.key))
+
     return (
         <div id={id} className="drum-pad" onClick={handleClick}>
             <p>{keySelect}</p>
@@ -22,4 +30,4 @@ const Pad = (props) => {
         </div>
     )
 }
-export default Pad
+export default Pad  
