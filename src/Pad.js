72821e0a8707ca607dadac71dee3170a5056f1c4
markoter <markoterdev@outlook.com>
2022-09-18 20:12:17 +0200

    use ref in pad to play audio

--- src/Pad.js
+++ src/Pad.js
@@ -3,10 +3,14 @@ import React from "react";
 const Pad = (props) => {
     const { sample, selectedPad, changeSelectedPad, handlePad, testerConsole, index } = props
 
+    const ref = React.useRef();
+    const playAudio = () => {
+        ref.current.play();
+    }
     return (
-        <div id={sample.name} className="drum-pad" onClick={handlePad}>
+        <div id={sample.name} className="drum-pad" onClick={playAudio}>
             <p>{sample.keyName}</p>
-            <audio src={sample.src} id={sample.keyName} className="clip" />
+            <audio ref={ref} src={sample.src} id={sample.keyName} className="clip" />
         </div>
     )
 }
